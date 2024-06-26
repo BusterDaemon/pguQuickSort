2c584800bea5f2aeda0fa0e17c8936bd8bcb82f3
Buster Daemon <bugsbunnygdi@pm.me>
2024-06-25 18:04:47 -0700

    Add project files.

---
+++ quickSort/MainForm.cpp
@@ -0,0 +1,13 @@
+﻿#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    quickSort::MainForm form;
+    Application::Run(% form);
+}
